ce77d1a0749e980c130ade92920afa4140b7ac07
Tassos <lambrou@gmail.com>
2017-12-19 22:47:36 -0600

    Minor updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { subscribeToTimer, toggleVote } from './api'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    subscribeToTimer((err, timestamp) => this.setState({
+      timestamp
+    }));
+
+    toggleVote( (currentVote) => this.setState({ vote: !currentVote }))
+
+    this.state = {
+      timestamp: 'no timestamp yet',
+      toggleVote: false
+    };
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is the timer value: {this.state.timestamp}
         </p>
+        <h3>
+          The current vote is set to: {this.state.toggleVote ? `ON` : 'OFF' }
+        </h3>
+        <button onClick={toggleVote((currentVote) => {this.setState({ vote: !currentVote })})}>Toggle Me</button>
       </div>
     );
   }
---
+++ src/api.js
@@ -0,0 +1,14 @@
+import openSocket from 'socket.io-client'
+const  socket = openSocket('http://localhost:8080')
+
+const subscribeToTimer = (cb) => {
+  socket.on('timer', timestamp => cb(null, timestamp))
+  socket.emit('subscribeToTimer', 1000)
+}
+
+const toggleVote = (voteCB) => {
+  socket.on('vote', vote => voteCB(vote))
+  socket.emit('toggleVote', false)
+}
+
+export { subscribeToTimer, toggleVote }

5cdb4a9942dfc7af49181941e61557246535734c
Tassos <lambrou@gmail.com>
2018-01-05 12:53:39 -0600

    bug fix

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client'
-const  socket = openSocket('http://localhost:8080')
+const socket = openSocket('http://localhost:8080')
 
 const subscribeToTimer = (cb) => {
   socket.on('timer', timestamp => cb(null, timestamp))
